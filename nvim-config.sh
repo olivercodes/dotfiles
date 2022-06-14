294c18b5a7ba8a3498c84e122d48dcb9656f96f0
olivercodes <boliver@linux.com>
2022-06-14 02:00:13 -0400

    starting over

--- nvim-config.sh
+++
@@ -1,7 +0,0 @@
-# Source init.vim to the config folder...
-
-DIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
-echo "$DIR"
-USER_HOME=$(eval echo ~$user)
-cd $DIR
-ln -fsv $DIR"/".dotfiles"/"init.vim $USER_HOME"/".config/nvim/
