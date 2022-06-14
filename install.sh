6cc115ce46d44e52b2b49cde1fc9dae24a4e62df
Bryan Oliver <boliver@linux.com>
2022-06-14 05:06:11 -0400

    zshrc

--- install.sh
+++ install.sh
@@ -114,6 +114,11 @@ echo "---------------------------------------------------------"
 echo "setup oh-my-zsh"
 ~/.dotfiles/ohmyzsh/tools/install.sh
 
+echo "setting zshrc back, cat .zshrc.pre-oh-my-zsh >> .zshrc"
+cat .zshrc.pre-oh-my-zsh >> .zshrc
+echo "you'll need to restart terminal for this to take effect"
+
+
 echo "---------------------------------------------------------"
 echo "All done!"
 echo "and change your terminal font to source code pro"
